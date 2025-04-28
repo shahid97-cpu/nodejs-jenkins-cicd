a25fb24b0c705b68e2db1c4d95ebdfda8ca5d139
shahid97-cpu <153984075+shahid97-cpu@users.noreply.github.com>
2025-04-28 12:34:11 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/about', (req, res) => res.sendFile(__dirname + '/public/about.html'));
+
+app.listen(3000, () => console.log(`App running on http://localhost:${port}`));
