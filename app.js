9a0d421adf13c907f917d4fce57004cb8c1e20b2
shahid97-cpu <153984075+shahid97-cpu@users.noreply.github.com>
2025-04-28 14:43:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ const port = 3000;
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 app.get('/about', (req, res) => res.sendFile(__dirname + '/public/about.html'));
 
-app.listen(3000, () => console.log(`App running on http://localhost:${port}`));
+app.listen(3000, () => console.log(`Listening on port 3000`);
+}); 
